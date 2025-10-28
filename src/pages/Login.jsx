0477da0e584595aaf30d4d286eff5e54f5b6c917
Joshua504 <chukwuebukajosh8@gmail.com>
2025-10-28 02:39:26 +0100

    v4

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
-import styles from "../styles/login.module.scss"
-import Logincomp from "../components/Logincomp"
+import styles from "../styles/login.module.scss";
+import Logincomp from "../components/Logincomp";
 import Signupcomp from "../components/Signupcomp";
+import av_3 from "../assets/img/Blue Background.gif";
 
 const Login = () => {
-  const [isLogin, setIsLogin] = useState(true);
+ const [isLogin, setIsLogin] = useState(true);
 
  return (
   <>
    <section className={styles.login}>
-    <div className={styles.login__logo}></div>
+    <div className={styles.login__logo}>
+     <img src={av_3} alt="" />
+     <h1>ZenDesk</h1>
+    </div>
     <div className={styles.login__forms}>
-     {isLogin ? <Logincomp onSwitch={() => setIsLogin(false)} /> : <Signupcomp onSwitch={() => setIsLogin(true)} />}
+     {isLogin ? (
+      <Logincomp onSwitch={() => setIsLogin(false)} />
+     ) : (
+      <Signupcomp onSwitch={() => setIsLogin(true)} />
+     )}
     </div>
    </section>
   </>
